9716768d5ae4a3effc2e725065182ecf435f28a9
Adam <aashelt90@gmail.com>
2018-09-29 18:41:09 -0400

    initial commit

---
+++ src/main/resources/archetype-resources/src/main/java/services/V1.java
@@ -0,0 +1,32 @@
+package ${package}.services;
+
+/**
+ * V1 CSIP service...
+ *
+ */
+import csip.ServiceException;
+import javax.ws.rs.Path;
+import static csip.annotations.State.*;
+import csip.annotations.*;
+
+@Name("conv #2")
+@Description("Example of an simple service")
+@Category("Examples")
+@Category("Climate")
+@Path("m/${artifactId}/1.0")
+public class V1 extends csip.ModelDataService {
+
+    @Override
+    protected void doProcess() throws Exception {
+        long sleep = getLongParam("sleep", 0);
+        Thread.sleep(sleep);
+        double temp = getDoubleParam("temp");
+        String un = getParamUnit("temp");
+        if (un.equals("C")) {
+            putResult("temp", temp * 9 / 5 + 32, "temperature", "F");
+            return;
+        }
+        throw new ServiceException("missing unit.");
+    }
+}
+
---
+++ src/main/resources/archetype-resources/src/main/java/util/Util.java
@@ -0,0 +1,13 @@
+package $package.util;
+
+/**
+ * V1 CSIP service...
+ *
+ */
+public class Util
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
